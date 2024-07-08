c6169a8022989e0c411023fdb83af71cc13da456
KitllyCat <cjchambem@unjbg.edu.pe>
2024-07-07 21:58:10 -0500

    Se agrego la funcion agregarContacto para la funcion principal

--- Ejercicio_01.cpp
+++ Ejercicio_01.cpp
@@ -9,7 +9,51 @@ d) Mostrar listado de contactos existentes, ordenado por servidor de correo de l
 e) Salir del programa
 El programa debe mantenerse en ejecución continuamente hasta que usuario indique que desea salir del mismo. */
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
+
+struct contactoEmail {
+    string nombres;
+    string sexo;
+    string edad;
+    string telefono;
+    string email;
+    string nacionalidad;
+};
+vector<contactoEmail> contactos;
+//minifuncion para que compruebe si el email tiene @ o no
+bool esEmailValido(const string& email) { 
+    return email.find('@') != string::npos;
+}
+//agregar contacto
+void agregarContacto(vector<contactoEmail>& contactos){
+	
+	contactoEmail nuevoContacto;
+    cout << "Ingrese los nombres completos del nuevo contacto: ";
+    cin.ignore();
+    getline(cin, nuevoContacto.nombres);
+    cout << "Ingrese el sexo (M/F): ";
+    cin >> nuevoContacto.sexo;
+    cout << "Ingrese la edad: ";
+    cin >> nuevoContacto.edad;
+    cout << "Ingrese el teléfono: ";
+    cin >> nuevoContacto.telefono;
+    do {
+        cout << "Ingrese el email: ";
+        cin >> nuevoContacto.email;
+        if (!esEmailValido(nuevoContacto.email)) {
+            cout << "El email es invalido. Debe contener minimo un '@'" << endl;
+        }
+    } while (!esEmailValido(nuevoContacto.email));
+    cout << "Ingrese la nacionalidad: ";
+    cin.ignore();
+    getline(cin, nuevoContacto.nacionalidad);
+    contactos.push_back(nuevoContacto);
+	cout<<"Contacto guardado safisfactoriamente~!"<<endl;
+}
+
 
 int main(){
 	int opcion;
@@ -26,11 +70,10 @@ int main(){
 		cout << endl;
 		switch(opcion){
 			case 0;
-			//salir del programa
 			break;
 			
 			case 1:
-			//aqui va la funcion de agregarContacto
+			agregarContacto(contactos);
 			break;
 			
 			case 2:
