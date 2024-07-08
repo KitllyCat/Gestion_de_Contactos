88094238d534a5bb8beb6ad644fd9f39bf481e4d
KitllyCat <cjchambem@unjbg.edu.pe>
2024-07-08 00:26:47 -0500

    Arreglando y corrigiendo errores de compilacion

--- Ejercicio_01.cpp
+++ Ejercicio_01.cpp
@@ -1,13 +1,3 @@
-/* 1.
-Gestión de Contactos
-Crear una estructura llamada contactoEmail que tenga las siguientes características: nombres completos, sexo, edad, teléfono, email, nacionalidad
-El programa debe constar de un menú que permita:
-a) Agregar un contacto
-b) Eliminar un contacto
-c) Mostrar listado general de contactos registrados hasta ese momento.
-d) Mostrar listado de contactos existentes, ordenado por servidor de correo de las cuentas (gmail.com, outlook.com, yahoo.com, etc.)
-e) Salir del programa
-El programa debe mantenerse en ejecución continuamente hasta que usuario indique que desea salir del mismo. */
 #include <iostream>
 #include <vector>
 #include <string>
@@ -38,7 +28,7 @@ void agregarContacto(vector<contactoEmail>& contactos){
     cin >> nuevoContacto.sexo;
     cout << "Ingrese la edad: ";
     cin >> nuevoContacto.edad;
-    cout << "Ingrese el teléfono: ";
+    cout << "Ingrese el telefono: ";
     cin >> nuevoContacto.telefono;
     do {
         cout << "Ingrese el email: ";
@@ -98,10 +88,10 @@ void mostrarContactoOrdenado(vector<contactoEmail>& contactos){
             cout << "Nombres: " << contacto.nombres << endl;
             cout << "Sexo: " << contacto.sexo << endl;
             cout << "Edad: " << contacto.edad << endl;
-            cout << "Teléfono: " << contacto.telefono << endl;
+            cout << "Telefono: " << contacto.telefono << endl;
             cout << "Email: " << contacto.email << endl;
             cout << "Nacionalidad: " << contacto.nacionalidad << endl;
-            cout << "-------------------------" << endl;
+            cout << endl;
         }
     }
 }
@@ -110,49 +100,54 @@ int main(){
 	int opcion;
 	do{
 		cout << "---Gestor de contactos---"<<endl;
-		cout << "Seleccione la acción que desea realizar" << endl;
-		cout << "A. Agregar un nuevo contacto" << endl;
-		cout << "B. Eliminar un contacto existente" << endl;
-		cout << "C. Mostrar todos los contactos guardados" << endl;
-		cout << "D. Mostrar todos los contactos guardados en orden de correos" << endl;
-		cout << "0. Salir" << endl;
+		cout << "Seleccione la opcion que desea realizar en el programa:" << endl;
+		cout << "(1) Agregar un nuevo contacto" << endl;
+		cout << "(2) Eliminar un contacto existente" << endl;
+		cout << "(3) Mostrar todos los contactos guardados" << endl;
+		cout << "(4) Mostrar todos los contactos guardados en orden de correos" << endl;
+		cout << "(0) Salir" << endl;
 		cout << "Selecione su opcion: ";
 		cin >> opcion;
 		cout << endl;
 		switch(opcion){
 			
-			case 1:
+			case 1:{
 			agregarContacto(contactos);
 			break;
+			}
 			
-			case 2:
+			case 2:{
             string nombre;
             cout << "Ingrese el nombre del contacto que desea eliminar: " << endl;
             cin.ignore();
             getline(cin, nombre);
             eliminarContacto(contactos, nombre);
 			break;
+			}
 		
-			case 3:
+			case 3:{
 			cout<<"Mostrando todos los contactos guardados: "<<endl;
 			cout<<endl;
 			mostrarContacto(contactos);
 			break;
+			}
 			
-			case 4:
-			cout<<"Mostrando los contactos guardados ordenadenadamente(por correo): "
-			mostrarContactoOrdenado(vector<contactoEmail>& contactos);
+			case 4:{
+			cout<<"Mostrando los contactos guardados ordenadenadamente(por correo): "<<endl;
+			mostrarContactoOrdenado(contactos);
 			cout<<endl;
 			break;
+			}
 			
-			case 0:
+			case 0:{
 			cout<<"Saliendo del programa..."<<endl;
 			break;
+			}
 			
-			default:
-			
+			default:{
 			cout << "Opción no válida. Intente nuevamente." << endl;
-			break;	
+			break;
+			}	
 					
 		}
 		cout << endl;
